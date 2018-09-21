13346a1bb2d86c7034ada524580540c7442eb0e1
Joanna Gora <joanna.gora@protonmail.com>
2018-09-21 20:09:51 +0100

    massive merge and recovery. what works: populated dropdown, checkboxes, server is running

--- client/src/app.js
+++ client/src/app.js
@@ -1,12 +1,24 @@
+const Request = require('./helpers/request');
+const PubSub = require('./helpers/pub_sub');
+const Jobs = require('./models/jobs');
+const JobsSelectView = require('./views/jobs_select_view.js')
+const FormView = require('./views/form_view.js')
 const Countries = require("./models/countries");
-//require dependencies
+
 document.addEventListener('DOMContentLoaded', () => {
-    // const countriesForm = document.querySelector('form#countries-form');
-    // const countriesFormView = new countriesFormView(countriesForm);
-    // countriesFormView.bindEvents();
+  const categoriesElement = document.querySelector('select#occupation-select')
+  const jobsSelectView = new JobsSelectView(categoriesElement);
+  jobsSelectView.bindEvents();
 
-    const countries = new Countries();
-    //countries.bindEvents();
-    countries.getData();
+  const jobs = new Jobs();
+  jobs.bindEvents();
 
-});
+  const preferencesForm = document.querySelector('#preferences-form');
+  const formView = new FormView(preferencesForm);
+  formView.bindEvents();
+
+  const countries = new Countries();
+  //countries.bindEvents();
+  countries.getData();
+
+})
