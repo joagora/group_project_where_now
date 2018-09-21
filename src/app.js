54776521781372d44b410729e1872c5ff877604e
Bea <beata.ficek@gmail.com>
2018-09-21 18:53:24 +0100

    populate dropdown

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
-//require dependencies
+const Request = require('./helpers/request');
+const PubSub = require('./helpers/pub_sub');
+const Jobs = require('./models/jobs');
+const JobsSelectView = require('./views/jobs_select_view.js')
+
 document.addEventListener('DOMContentLoaded', () => {
+  const categoriesElement = document.querySelector('select#occupation-select')
+  const jobsSelectView = new JobsSelectView(categoriesElement);
+  jobsSelectView.bindEvents();
+
+  const jobs = new Jobs();
+  jobs.bindEvents();
 
 })
