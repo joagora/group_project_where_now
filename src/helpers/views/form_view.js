90cc77d77b1da1202c787fee50f1c90fd020855b
Gemma <gemma.percival@hotmail.co.uk>
2018-09-21 18:08:33 +0100

    wrote meathod for getting checked checkboxes

---
+++ src/helpers/views/form_view.js
@@ -0,0 +1,35 @@
+const PubSub = require('../helpers/pub_sub');
+
+const FormView = function(formContainer) {
+  this.formContainer = formContainer;
+}
+
+FormView.prototype.bindEvents = function() {
+  this.formContainer.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const completedForm = this.getSubmittedValues(event.target);
+    PubSub.publish('FormView:form-submitted', completedForm)
+  })
+}
+
+FormView.prototype.getSubmittedValues = function(form) {
+  const checkboxes = this.grabCheckboxes(form);
+  const checkedCheckboxes = this.getCheckedCheckboxes(checkboxes)
+}
+
+FormView.prototype.grabCheckboxes = function(form) {
+  const checkboxes = document.querySelectorAll('.preference-button')
+  return checkboxes
+}
+
+FormView.prototype.getCheckedCheckboxes = function(checkboxes) {
+  const checkboxArray = Array.form(checkboxes);
+  return checkboxArray.filter((checkbox) => {
+    return checkbox.checked === true;
+  })
+}
+
+
+//TODO: map the values , return array
+
+module.exports = FormView;
