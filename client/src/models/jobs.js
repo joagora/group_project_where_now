52c69492f400678305c348c9da1bb4837c3d193b
Joanna Gora <joanna.gora@protonmail.com>
2018-09-24 11:11:01 +0100

    change salary api call to teleport

--- client/src/models/jobs.js
+++ client/src/models/jobs.js
@@ -4,7 +4,7 @@ const ADZUNA_API_KEY = require('../helpers/api_keys/adzuna_api_key.js');
 const ADZUNA_ID = require('../helpers/api_keys/adzuna_id.js');
 
 const Jobs = function(){
-  this.jobsData = null;
+  this.jobsSalaries = null;
 }
 
 Jobs.prototype.bindEvents = function(){
@@ -12,18 +12,17 @@ Jobs.prototype.bindEvents = function(){
 }
 
 Jobs.prototype.getData = function(){
-  const url = `http://api.adzuna.com/v1/api/jobs/gb/categories?app_id=${ADZUNA_ID}&app_key=${ADZUNA_API_KEY}&&content-type=application/json`;
+  const url = `https://api.teleport.org/api/countries/iso_alpha2:PL/salaries/`;
   const request = new Request(url);
   request.get()
     .then((data) => {
-      this.jobsData = data;
+      this.jobsSalaries = data.salaries;
     })
     .then((data) => {
-      const categoriesDetails = this.jobsData.results;
-      const categoriesLabels = categoriesDetails.map((category) => {
-        return category.label;
+      const jobTitles = this.jobsSalaries.map((jobElement) => {
+        return jobElement.job.title;
       })
-      PubSub.publish('Jobs:categories-labels-ready', categoriesLabels);
+      PubSub.publish('Jobs:categories-labels-ready', jobTitles);
     })
 }
 
