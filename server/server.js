bab0ecf230a2e010185bfba9909bf1a1c2d2dbde
Joanna Gora <joanna.gora@protonmail.com>
2018-10-04 14:39:09 +0100

    add config for dotenv

--- server/server.js
+++ server/server.js
@@ -3,8 +3,11 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
-const NUMBEO_API_KEY = require('./helpers/api_keys/numbeo_api_key.js');
-const GEOCODE_API_KEY = require('./helpers/api_keys/geocoding_api_key.js');
+const dotenv = require('dotenv');
+dotenv.load();
+const numbeoKey = process.env.NUMBEO_API_KEY;
+console.log(numbeoKey);
+// const GEOCODE_API_KEY = require('./helpers/api_keys/geocoding_api_key.js');
 const publicPath = path.join(__dirname, '../client/public');
 const createRouter = require('./helpers/create_router.js');
 
@@ -16,8 +19,9 @@ app.get('/', (req, res) => {
 });
 
 app.get('/numbeo', (req, res) => {
+
   const countryName =  req.query.countryName;
-  const url = `https://www.numbeo.com/api/country_indices?api_key=${NUMBEO_API_KEY}&country=${countryName}`;
+  const url = `https://www.numbeo.com/api/country_indices?api_key=${numbeoKey}&country=${countryName}`;
   fetch(url)
    .then(jsonData => jsonData.json())
    .then(data => res.json(data));
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
+const Dotenv = require('dotenv-webpack');
 const config = {
   entry: `${__dirname}/client/src/app.js`,
   output: {
     path: `${__dirname}/client/public/js`,
     filename: 'bundle.js'
   },
-  mode: 'development'
+  mode: 'development',
+  node: {
+       fs: 'empty'
+     },
+     plugins: [
+       new Dotenv()
+     ]
 };
 
 module.exports = config;
