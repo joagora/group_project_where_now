d37d029ce0d97312281b61508e8dfd7d77e13bfd
Joanna Gora <joanna.gora@protonmail.com>
2018-10-07 23:22:02 +0100

    adjusted mobile size

--- client/src/models/countries_properties.js
+++ client/src/models/countries_properties.js
@@ -16,8 +16,8 @@ CountriesProperties.prototype.getData = function(countries) {
   this.countriesList = countries;
   const countryProperties = countries.forEach((country) => {
     const countryName = country.name;
-    // const url = `http://localhost:3000/numbeo?countryName=${countryName}`;
-    const url = `https://where-now-app.herokuapp.com/numbeo?countryName=${countryName}`;
+    const url = `http://localhost:3000/numbeo?countryName=${countryName}`;
+    // const url = `https://where-now-app.herokuapp.com/numbeo?countryName=${countryName}`;
     const request = new Request(url);
     request.get()
       .then((data) => {
