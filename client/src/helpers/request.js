9f4c90c61c6e61ef43f6bf040778e77fc2111125
Joanna Gora <joanna.gora@protonmail.com>
2019-06-22 14:05:21 +0100

    add cors proxy

--- client/src/helpers/request.js
+++ client/src/helpers/request.js
@@ -1,26 +1,20 @@
 const Request = function (url) {
   this.url = url;
+  this.proxyurl = "https://cors-anywhere.herokuapp.com/";
 };
 
 Request.prototype.get = function () {
-  return fetch(this.url)
+  return fetch(this.proxyurl + this.url)
     .then((response) => response.json());
 };
 
 Request.prototype.post = function (payload) {
-  return fetch(this.url, {
+  return fetch(this.proxyurl + this.url, {
     method: 'POST',
     body: JSON.stringify(payload),
     headers: { 'Content-Type': 'application/json' }
   })
     .then((response) => response.json());
 };
-
-// Request.prototype.delete = function (id) {
-//   return fetch(`${this.url}/${id}`, {
-//     method: 'DELETE'
-//   })
-//     .then((response) => response.json());
-// };
 
 module.exports = Request;
